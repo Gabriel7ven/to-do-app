52ce371bb97a3d2a17858daefd7fe91c4baf10b1
Gabrie7ven <gabriel.oficial@yahoo.com>
2026-07-12 13:27:03 -0300

    .

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -11,9 +11,8 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__) #, instance_relative_config=True)
- 
+
+    app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -25,26 +24,15 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    #os.makedirs(app.instance_path, exist_ok=True)
 
-    # a simple page that says hello
-    @app.route('/hello/')
-    def hello():
-        return 'Hello, World!'
     
 
-    # from . import db
     from . import auth
     from . import notes
-
-    # db.init_app(app)
     
     app.register_blueprint(auth.bp)
     app.register_blueprint(notes.bp)
